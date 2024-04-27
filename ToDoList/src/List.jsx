3192fee8134bd076085fb78b8623540b0ce84326
Alain Cruz Lozano <isc20350262@gmail.com>
2024-04-26 18:47:06 -0600

    new

---
+++ ToDoList/src/List.jsx
@@ -0,0 +1,30 @@
+import ListItem from "./ListItem"
+const List=({tasklist})=>{
+
+return (  
+    <>   
+    <div className="row">
+      <div className="col-6">
+        <h4> Tasks</h4>
+      </div>
+      <div className="col-3">
+        <h4>Limit </h4>
+      </div>
+      <div className="col">
+        <h4>Location</h4>
+      </div>
+    </div>
+    {
+        tasklist.map((task)=>(
+                <ListItem
+                key={task.id}
+                task={task}
+                />          
+        )
+    )
+    }
+    </>
+  )
+}
+
+export default List
---
+++ ToDoList/src/ListItem.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import ShowItemModal from "./ShowItemModal"
+const ListItem  =({task})=>{
+    return(
+    <>
+    <div className="row m-2">
+    <div className="col-6">
+        <ShowItemModal task={task}/>
+    <button className="btn btn-link"
+    data-bs-toggle="modal"
+    data-bs-target={"#showItemModal" +task.id}
+    >{task.task}</button>
+    </div>
+<div className='col-3'>
+{task.limit}
+</div>
+<div className='col'>
+{task.location}
+
+</div>
+</div>
+</>
+    )
+}
+export default ListItem
